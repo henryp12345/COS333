2749bfe207c2a3c2d4003af2128c4f0c77612a1c
Henry Peters <henryp109@gmail.com>
2019-04-07 14:04:21 -0400

    views test

--- poster/urls.py
+++ poster/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from posterApp import views
 
 urlpatterns = [
-	path('test/', include('posterApp.urls')),
+	path('', views.default),
+	path('event/', views.test),
     path('admin/', admin.site.urls),
 ]
--- posterApp/views.py
+++ posterApp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def default(request):
+	return HttpResponse("default page")
 def test(request):
 	return HttpResponse("this is a test")
