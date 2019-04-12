8b1a485cf119095e1e77624b7f1663a221a98d8f
Henry Peters <henryp109@gmail.com>
2019-04-12 11:04:47 -0400

    more views

--- posterApp/views.py
+++ posterApp/views.py
@@ -8,16 +8,16 @@ def default(request):
 	return HttpResponse("default page")
 
 def event(request):
-	# if request.method == 'GET':
-	# 	values = Event.objects.all().values()
-	# 	values_list = list(values)
-	# 	return JsonResponse(values_list, safe=False)
 	if request.method == 'GET':
+		values = Event.objects.all().values()
+		values_list = list(values)
+		return JsonResponse(values_list, safe=False)
+	elif request.method == 'POST':
 		formData = json.loads(request.body)
 		e1 = Event(title = formData.title, desc = formData.desc, location = formData.location, startTime = None, endTime = None,
 					date = None, capacity = formData.capacity, numberJoined = 0, tags = "", host = "")
 		e1.save()
-		return HttpResponse(formData)
+		return JsonResponse(request.body)
 
 def eventId(request):
 	return HttpResponse("nothing here yet")
