c956509c95866dc2d16cc575c790807327f1474c
Henry Peters <henryp109@gmail.com>
2019-04-12 11:26:08 -0400

    more views

--- posterApp/views.py
+++ posterApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import User, Event
 import json
 
@@ -7,17 +8,18 @@ import json
 def default(request):
 	return HttpResponse("default page")
 
+@csrf_exempt
 def event(request):
 	if request.method == 'GET':
 		values = Event.objects.all().values()
 		values_list = list(values)
 		return JsonResponse(values_list, safe=False)
 	elif request.method == 'POST':
-		formData = json.loads(request.body)
-		e1 = Event(title = formData.title, desc = formData.desc, location = formData.location, startTime = None, endTime = None,
-					date = None, capacity = formData.capacity, numberJoined = 0, tags = "", host = "")
-		e1.save()
-		return JsonResponse(json.dumps(formData))
+		# formData = json.loads(request.body)
+		# e1 = Event(title = formData.title, desc = formData.desc, location = formData.location, startTime = None, endTime = None,
+		# 			date = None, capacity = formData.capacity, numberJoined = 0, tags = "", host = "")
+		# e1.save()
+		return HttpResponse("OK")
 
 def eventId(request):
 	return HttpResponse("nothing here yet")
