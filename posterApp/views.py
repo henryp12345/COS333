82bcfd5f3be536a2de1679c3efb7ba0df5241af0
Henry Peters <henryp109@gmail.com>
2019-04-12 09:38:56 -0400

    clear tables page

--- posterApp/views.py
+++ posterApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import User, Event
 
 # Create your views here.
@@ -8,14 +8,11 @@ def default(request):
 
 def event(request):
 	e1 = User(username='test', hosted = '1', joined='2,3', notifications='1', invitations='2')
-	e2 = User(username='test2', hosted = '3', joined='4,5', notifications='2', invitations='4')
 	e1.save()
-	e2.save()
 	q = User.objects.all()
-	value = ''
-	for item in q:
-		value = value + " " + item.username + '\n'
-	return HttpResponse(value)
+	values = User.objects.all().values()
+	values_list = list(values)
+	return JsonResponse(values_list, safe=False)
 
 def eventId(request):
 	return HttpResponse("nothing here yet")
