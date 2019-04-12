1938af673fe7f46cac0f8d7bb21ade8bf80299af
Henry Peters <henryp109@gmail.com>
2019-04-12 10:40:16 -0400

    more views

--- posterApp/views.py
+++ posterApp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import User, Event
+import json
 
 # Create your views here.
 def default(request):
 	return HttpResponse("default page")
 
 def event(request):
-	e1 = User(username='test', hosted = '1', joined='2,3', notifications='1', invitations='2')
-	e1.save()
-	q = User.objects.all()
-	values = User.objects.all().values()
-	values_list = list(values)
-	return JsonResponse(values_list, safe=False)
+	if request.method == 'GET':
+		values = User.objects.all().values()
+		values_list = list(values)
+		return JsonResponse(values_list, safe=False)
+	elif request.method == 'POST':
+		formData = json.loads(request.body)
+		e1 = Events(title = formData.title, desc = formData.desc, location = formData.location, startTime = None, endTime = None,
+					date = None, capacity = formData.capacity, numberJoined = 0, tags = "", host = "")
+		e1.save()
+		return HttpResponse("OK")
 
 def eventId(request):
 	return HttpResponse("nothing here yet")
