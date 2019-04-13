d4b61bfabe4902bf99d46d69f8da8901e87fe62b
Henry Peters <henryp109@gmail.com>
2019-04-13 13:05:36 -0400

    more views

--- posterApp/views.py
+++ posterApp/views.py
@@ -27,12 +27,17 @@ def eventId(request, eventId):
 	event_list = list(event)
 	return JsonResponse(event_list, safe=False)
 
+@csrf_exempt
 def hosted(request, username):
 	if request.method == 'GET':
+		u1 = User(username="asd", hosted="1,2,3", joined="4,5,6", notifications="1,2,3", invitations="")
+		u1.save()
 		user = User.objects.get(username = username)
 		eventsHosted = user.hosted
-		return HttpResponse(type(eventsHosted))
+		hostedList = eventsHosted.split(",")
+		return JsonResponse(hostedList, safe=False)
 	elif request.method == 'POST':
+		return HttpResponse("not implemented")
 
 def joined(request, username):
 	return HttpResponse("nothing here yet")
