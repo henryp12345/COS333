315d407a1e4a9c7d70cbd788e74a68e9aae7af2f
Henry Peters <henryp109@gmail.com>
2019-04-14 19:43:24 -0400

    test

--- poster/settings.py
+++ poster/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# COMMENT OUT TO CHANGE DATABASE STUFF
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# COMMENT OUT TO CHANGE DATABASE STUFF
+django_heroku.settings(locals())
