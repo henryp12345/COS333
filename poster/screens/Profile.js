eeccca2c9b604e4a5e0c04320749b7186280549f
Kat Wang <kjwang@princeton.edu>
2019-04-27 21:07:48 -0400

    events hosted/joined

--- poster/screens/Profile.js
+++ poster/screens/Profile.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
-import { Card, Icon } from 'react-native-elements';
-import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
-import Chatroom from './Chatroom';
+import { Text, View, StyleSheet, FlatList, ActivityIndicator, Platform, TouchableOpacity, ScrollView } from 'react-native';
+import { SearchBar, Button, Icon } from 'react-native-elements';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { iOSUIKit } from 'react-native-typography'
+import { Card } from 'native-base';
+import { LinearGradient } from 'expo';
 
-class Profile extends React.Component {
+export default class Profile extends Component {
 
   constructor(props) {
     super(props)
@@ -33,55 +26,141 @@ class Profile extends React.Component {
   componentDidUpdate() {
     
   }
+
+  ListViewItemSeparator = () => {
+    return (
+      <View
+      style={{
+        height: 0.3,
+        width: '90%',
+        backgroundColor: '#080808',
+      }}
+      />
+      );
+  };
   
   render() {
     return (
+      <View style={styles.viewStyle}>
+      <ScrollView>
       //ListView to show with textinput used as search bar
       <View style={styles.viewStyle}>
-      <Text style={styles.customTitle}>Welcome back, Henry.</Text>
+      <Text style={styles.customTitle}>{"Welcome back, "}{"Henry"}.</Text>
       <Text style={styles.customSubtitle}>Events You are Hosting</Text>
       <FlatList
         data={this.state.hosted}
         renderItem={({ item }) => (
-          <Card>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={styles.textStyle}>{item.title}</Text>
-            <TouchableOpacity style={styles.textStyle}
+          <Card style={{borderRadius: 10}}>
+        <View style={styles.itemContent}>
+        <View style={styles.iconContainer}>
+          <Text style={styles.dateText}>{item.startDate.substring(5,7)}/{item.startDate.substring(8,10)}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.eventTitle}>{item.title}</Text>
+            <Text style={{marginRight: 10}}>Posted by {item.host}</Text>
+            <Text style={{marginRight: 10}}>{item.capacity-item.numberJoined} of {item.capacity} slots available</Text>
+          </View>
+
+          <View style={styles.downTextContainer}>
+            <View style={styles.downTextContainerInner}>
+              <View style={styles.downText}>
+                <View style={styles.priceText}>
+                </View>
+
+              <View style={styles.label}>
+
+              <TouchableOpacity style={styles.textStyle}
               onPress={() => this.props.navigation.navigate("Room", {topic: "React Navigation", roomId: item.chatroom, userId: this.state.userId})}>
               <Icon name='comment'
                     type='font-awesome'
                     color='#00bfff'
                     size={30}/>
-            </TouchableOpacity>
+              </TouchableOpacity>
+
+              </View>
+              </View>
+            </View>
           </View>
-          </Card>
+        </View>
+        </View>
+        </Card>
         )}
-        keyExtractor={(item, index) => index.toString()} />
+        enableEmptySections={true}
+        style={{ marginTop: 10 }}
+        keyExtractor={(item, index) => index.toString()}/>
+      
       <Text style={styles.customSubtitle}>Events You Have Joined</Text>
+
       <FlatList
         data={this.state.joined}
         renderItem={({ item }) => (
-          <Card>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={styles.textStyle}>{item.title}</Text>
-            <TouchableOpacity style={styles.textStyle}
+
+        <Card style={{borderRadius: 10}}>
+        <View style={styles.itemContent}>
+        <View style={styles.iconContainer}>
+          <Text style={styles.dateText}>{item.startDate.substring(5,7)}/{item.startDate.substring(8,10)}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.eventTitle}>{item.title}</Text>
+            <Text style={{marginRight: 10}}>Posted by {item.host}</Text>
+            <Text style={{marginRight: 10}}>{item.capacity-item.numberJoined} of {item.capacity} slots available</Text>
+          </View>
+
+          <View style={styles.downTextContainer}>
+            <View style={styles.downTextContainerInner}>
+              <View style={styles.downText}>
+                <View style={styles.priceText}>
+                </View>
+
+              <View style={styles.label}>
+
+              <TouchableOpacity style={styles.textStyle}
               onPress={() => this.props.navigation.navigate("Room", {topic: "React Navigation", roomId: item.chatroom, userId: this.state.userId})}>
               <Icon name='comment'
                     type='font-awesome'
                     color='#00bfff'
                     size={30}/>
-            </TouchableOpacity>
+              </TouchableOpacity>
+
+              </View>
+              </View>
+            </View>
           </View>
-          </Card>
+        </View>
+        </View>
+        </Card>
         )}
-        keyExtractor={(item, index) => index.toString()} />
+        enableEmptySections={true}
+        style={{ marginTop: 10 }}
+        keyExtractor={(item, index) => index.toString()}/>
+
+      </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  customTitle: {
+  viewStyle: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    flex: 1,
+    margin: 20,
+    margin: 15,
+    textAlign: 'center',
+  },
+  TextViewStyle:
+  {
+   borderWidth: 1, 
+   borderRadius: 10,
+   borderColor: '#E91E63',
+   width: '80%',
+   padding: 5,
+   backgroundColor: '#FFEB3B'
+ },
+ customTitle: {
   ...iOSUIKit.largeTitleEmphasizedObject,
   fontSize: 28,
 },
@@ -90,33 +169,81 @@ customSubtitle: {
   fontSize: 20,
 },
 
-  viewStyle: {
-    flex: 1,
-    backgroundColor: 'white',
-    margin: 20,
-    margin: 15,
-    textAlign: 'center',
-  },
+customSubtitle2: {
+  ...iOSUIKit.title3Object,
+  fontSize: 14,
+},
 
-  textStyle: {
-    padding: 10,
-    alignItems: 'flex-start',
-    padding:30,
-  },
+eventTitle: {
+  ...iOSUIKit.subheadEmphasizedObject,
+  fontSize: 18,
+},
+
+dateText: {
+  ...iOSUIKit.subheadEmphasizedObject,
+  fontSize: 18,
+  color: 'white',
+  textAlign: 'center',
+
+},
+
+textStyle: {
+  padding: 5,
+  textAlign: 'left',
+},
+
+rightText: {
+  textAlign: 'right',
+},
+container: {
+  marginBottom: 15,
+},
+
+itemContent:  {
+ flexDirection: 'row',
+ borderBottomWidth: 1,
+ borderColor: '#e5e5e5',
+ minHeight: 75
+},
+
+iconContainer: {
+  padding: 5,
+  flex: 1,
+  borderRadius: 5,
+  backgroundColor: '#558fed',
+  justifyContent: 'center',
+},
+
+textContainer: {
+  padding: 10,
+  flex: 5,
+},
+
+downTextContainer: {
+  flex: 6,
+  position: 'relative',
+},
+
+downTextContainerInner: {
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+},
+
+downText: {
+  marginTop: 10,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+},
+
+priceText: {
+  flexDirection: 'row',
+},
+
+label: {
+  textAlign: 'right',
+  padding: 3
+}
+
 });
-
-
-const AppNavigator = createStackNavigator({
-  Home: {screen: Profile,
-         navigationOptions: {
-          header: null
-         }
-        },
-  Room: {screen: Chatroom,
-         navigationOptions: {
-          header: null
-         }
-        },
-});
-
-export default createAppContainer(AppNavigator);
